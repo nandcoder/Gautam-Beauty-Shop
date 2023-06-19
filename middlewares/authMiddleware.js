a9f7d28a343da048ce891e022e2bd4c49723a8ea
Gautam Rana <92631530+gsmartGautam@users.noreply.github.com>
2023-06-19 12:56:32 +0530

    Update authMiddleware.js

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -1,5 +1,5 @@
 import JWT from "jsonwebtoken";
-import userModel from "../models/usermodel.js";
+import userModel from "../models/userModel.js";
 
 //Protected Routes token base
 export const requireSignIn = async (req, res, next) => {
